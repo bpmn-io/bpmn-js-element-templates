e4d6c3950fc3380ed849d283da23f6de2b712069
Martin Stamm <martin.stamm@camunda.com>
2023-06-13 12:37:15 +0200

    fix: export CloudElementTemplatesValidator

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 export { default as CloudElementTemplatesPropertiesProviderModule } from './cloud-element-templates';
 export { default as ElementTemplatesPropertiesProviderModule } from './element-templates';
+
+// utils
+export { Validator as CloudElementTemplatesValidator } from './cloud-element-templates/Validator';
