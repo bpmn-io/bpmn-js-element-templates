3c6a1f1c207b91854bf4a1f650072d5110e6240d
Vovamzur <vovamazur3@gmail.com>
2024-03-25 15:53:49 +0200

    fix missing applying of the `translate` service

--- test/spec/element-templates/i18n/Translate.spec.js
+++ test/spec/element-templates/i18n/Translate.spec.js
@@ -6,12 +6,12 @@ import {
 } from 'min-dom';
 
 import {
-  bootstrapModeler,
+  bootstrapPropertiesPanel,
   inject
 } from 'test/TestHelper';
 
 import {
-  act,
+  act
 } from '@testing-library/preact';
 
 import coreModule from 'bpmn-js/lib/core';
@@ -24,9 +24,7 @@ import diagramXML from './Translate.bpmn';
 
 import templates from './Translate.templates';
 
-import customTranslate from './customTranslate';
-
-describe('provider/element-templates - ElementTemplates', function() {
+describe('provider/element-templates - Translate', function() {
 
   let container;
 
@@ -34,7 +32,7 @@ describe('provider/element-templates - ElementTemplates', function() {
     container = TestContainer.get(this);
   });
 
-  beforeEach(bootstrapModeler(diagramXML, {
+  beforeEach(bootstrapPropertiesPanel(diagramXML, {
     container,
     modules: [
       BpmnPropertiesPanelModule,
@@ -42,7 +40,12 @@ describe('provider/element-templates - ElementTemplates', function() {
       elementTemplatesModule,
       modelingModule,
       {
-        translate: [ 'value', customTranslate ]
+        translate: [
+          'value',
+          (template, replacements) => replacements
+            ? `${template}:translated:${JSON.stringify(replacements)}`
+            : `${template}:translated`
+        ]
       }
     ],
     moddleExtensions: {
@@ -51,20 +54,68 @@ describe('provider/element-templates - ElementTemplates', function() {
     elementTemplates: templates
   }));
 
-  describe('translate', function() {
-    it('should translate', inject(
-      async function(elementRegistry, elementTemplates, selection) {
+  beforeEach(inject(async function(elementRegistry, selection) {
+    const element = elementRegistry.get('ServiceTask_1');
+    await act(() => selection.select(element));
+  }));
 
-        const element = elementRegistry.get('ServiceTask_1');
+  describe('Template group', function() {
+    it('should apply the `translate` service for the Template group title', inject(function() {
+      const titleEl = domQuery('[data-group-id="group-ElementTemplates__Template"] .bio-properties-panel-group-header-title', container);
 
-        await act(() => {
-          selection.select(element);
-        });
+      expect(titleEl.textContent).to.equal('Template:translated');
+    }));
+  });
 
-        const groups = domQueryAll('[data-group-id]', container);
+  describe('Custom Properties group', function() {
+    it('should apply the `translate` service for the Custom Properties group title', inject(function() {
+      const titleEl = domQuery('[data-group-id="group-ElementTemplates__CustomProperties"] .bio-properties-panel-group-header-title', container);
 
-        console.log(groups.length);
-      }
-    ));
+      expect(titleEl.textContent).to.equal('Custom properties:translated');
+    }));
+
+    it('should apply the `translate` service for a Boolean entry', inject(function() {
+      const booleanEntry = domQuery('[data-group-id="group-ElementTemplates__CustomProperties"] [data-entry-id="custom-entry-foo-0"]', container);
+
+      const labelEl = domQuery('.bio-properties-panel-label', booleanEntry);
+      expect(labelEl.textContent).to.equal('BooleanLabel:translated');
+
+      const descriptionEl = domQuery('.bio-properties-panel-description', booleanEntry);
+      expect(descriptionEl.textContent).to.equal('BooleanDescription:translated');
+    }));
+
+    it('should apply the `translate` service for a Dropdown entry', inject(function() {
+      const dropdownEntry = domQuery('[data-group-id="group-ElementTemplates__CustomProperties"] [data-entry-id="custom-entry-foo-1"]', container);
+
+      const labelEl = domQuery('.bio-properties-panel-label', dropdownEntry);
+      expect(labelEl.textContent).to.equal('DropdownLabel:translated');
+
+      const descriptionEl = domQuery('.bio-properties-panel-description', dropdownEntry);
+      expect(descriptionEl.textContent).to.equal('DropdownDescription:translated');
+
+      const options = domQueryAll('option', dropdownEntry);
+      expect(options[0].textContent).to.equal('Choice1:translated');
+      expect(options[1].textContent).to.equal('Choice2:translated');
+    }));
+
+    it('should apply the `translate` service for a String entry', inject(function() {
+      const stringEntry = domQuery('[data-group-id="group-ElementTemplates__CustomProperties"] [data-entry-id="custom-entry-foo-2"]', container);
+
+      const labelEl = domQuery('.bio-properties-panel-label', stringEntry);
+      expect(labelEl.textContent).to.equal('StringLabel:translated');
+
+      const descriptionEl = domQuery('.bio-properties-panel-description', stringEntry);
+      expect(descriptionEl.textContent).to.equal('StringDescription:translated');
+    }));
+
+    it('should apply the `translate` service for a Text entry', inject(function() {
+      const textEntry = domQuery('[data-group-id="group-ElementTemplates__CustomProperties"] [data-entry-id="custom-entry-foo-3"]', container);
+
+      const labelEl = domQuery('.bio-properties-panel-label', textEntry);
+      expect(labelEl.textContent).to.equal('TextLabel:translated');
+
+      const descriptionEl = domQuery('.bio-properties-panel-description', textEntry);
+      expect(descriptionEl.textContent).to.equal('TextDescription:translated');
+    }));
   });
 });
--- test/spec/element-templates/i18n/customTranslate.js
+++
@@ -1,5 +0,0 @@
-export default function customTranslate(template, replacements) {
-  console.debug({ template, replacements });
-
-  return template;
-}
