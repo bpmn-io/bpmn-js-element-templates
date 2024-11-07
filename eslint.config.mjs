1f59c7721222e1735c0046b956aad737a0768584
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-11-07 02:07:28 +0100

    chore: update linter config to eslint@9

---
+++ eslint.config.mjs
@@ -0,0 +1,85 @@
+import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+
+const files = {
+  build: [
+    '*.js',
+    '*.mjs',
+    'test/distro/distro.spec.js'
+  ],
+  test: [
+    'test/**/*.js'
+  ],
+  ignored: [
+    'dist'
+  ]
+};
+
+
+export default [
+  {
+    ignores: files.ignored
+  },
+
+  // build
+  ...bpmnIoPlugin.configs.node.map(config => {
+    return {
+      ...config,
+      files: files.build
+    };
+  }),
+
+  // lib + test
+  ...bpmnIoPlugin.configs.browser.map(config => {
+    return {
+      ...config,
+      ignores: files.build
+    };
+  }),
+  ...bpmnIoPlugin.configs.jsx.map(config => {
+    return {
+      ...config,
+      ignores: files.build
+    };
+  }),
+
+  // misc
+  {
+    plugins: {
+      'react-hooks': reactHooksPlugin,
+      import: importPlugin
+    },
+    rules: {
+      ...reactHooksPlugin.configs.recommended.rules,
+      'import/first': 'error',
+      'import/no-amd': 'error',
+      'import/no-webpack-loader-syntax': 'error',
+      'react-hooks/exhaustive-deps': 'off'
+    },
+    ignores: files.build
+  },
+  {
+    rules: {
+      'react/display-name': 'off',
+      'react/no-unescaped-entities': 'off'
+    }
+  },
+
+  // test
+  ...bpmnIoPlugin.configs.mocha.map(config => {
+    return {
+      ...config,
+      files: files.test
+    };
+  }),
+  {
+    languageOptions: {
+      globals: {
+        sinon: true,
+        require: true
+      },
+    },
+    files: files.test
+  }
+];
