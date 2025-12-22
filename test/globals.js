a0badc72d92c236f97ed4d4ca94f9196c121bed7
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-12-22 22:33:12 +0100

    chore: move chai `jsonEqual` extension to globals

--- test/globals.js
+++ test/globals.js
@@ -1,5 +1,26 @@
 import { use } from 'chai';
 import sinonChai from 'sinon-chai';
 
+function jsonEqual(chai, utils) {
+
+  utils.addMethod(chai.Assertion.prototype, 'jsonEqual', function(comparison) {
+
+    var actual = JSON.stringify(this._obj);
+    var expected = JSON.stringify(comparison);
+
+    this.assert(
+      actual == expected,
+      'expected #{this} to deep equal #{act}',
+      'expected #{this} not to deep equal #{act}',
+      comparison, // expected
+      this._obj, // actual
+      true // show diff
+    );
+  });
+}
+
 // eslint-disable-next-line react-hooks/rules-of-hooks
 use(sinonChai);
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+use(jsonEqual);
