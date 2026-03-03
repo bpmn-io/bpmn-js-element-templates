66780b9ff202fd6afae223893326c37a774c2ce6
Adam Urban <adam.urban@gmail.com>
2026-03-03 12:03:01 +0100

    feat: improve field length error message (#225)

--- test/spec/element-templates/properties/CustomProperties.spec.js
+++ test/spec/element-templates/properties/CustomProperties.spec.js
@@ -1555,7 +1555,7 @@ describe('provider/element-templates - CustomProperties', function() {
                 input = domQuery(selector, entry);
 
           // assume
-          expectError(entry, 'Must have min length 5.');
+          expectError(entry, 'Field must be at least 5 characters.');
 
           // when
           changeInput(input, 'FOOOOOOO');
@@ -1580,7 +1580,7 @@ describe('provider/element-templates - CustomProperties', function() {
           changeInput(input, 'FOOOOOOO');
 
           // then
-          expectError(entry, 'Must have max length 5.');
+          expectError(entry, 'Field cannot exceed 5 characters.');
         });
 
 
