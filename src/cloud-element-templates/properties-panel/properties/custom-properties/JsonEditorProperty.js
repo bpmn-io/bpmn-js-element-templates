c14e815bb606250d5904bcc5dc550eeb39fa8625
AlekseyManetov <aleksey.manetov@camunda.com>
2026-04-24 17:01:07 +0200

    feat: use JSON editor for custom template properties with `language=json`
    Related to https://github.com/camunda/camunda-modeler/issues/4683

---
+++ src/cloud-element-templates/properties-panel/properties/custom-properties/JsonEditorProperty.js
@@ -0,0 +1,44 @@
+import { useService } from 'bpmn-js-properties-panel';
+import { PropertyDescription } from '../../../../components/PropertyDescription';
+import { PropertyTooltip } from '../../components/PropertyTooltip';
+import { JsonEditorEntry } from '@bpmn-io/properties-panel';
+import {
+  propertyGetter,
+  propertySetter,
+  propertyValidator
+} from './util';
+
+export function JsonEditorProperty(props) {
+  const {
+    element,
+    id,
+    property
+  } = props;
+
+  const {
+    description,
+    editable,
+    label,
+    placeholder,
+    tooltip
+  } = property;
+
+  const bpmnFactory = useService('bpmnFactory'),
+        commandStack = useService('commandStack'),
+        debounce = useService('debounceInput'),
+        translate = useService('translate');
+
+  return JsonEditorEntry({
+    debounce,
+    element,
+    id,
+    label,
+    placeholder,
+    description: PropertyDescription({ description }),
+    getValue: propertyGetter(element, property),
+    setValue: propertySetter(bpmnFactory, commandStack, element, property),
+    validate: propertyValidator(translate, property),
+    disabled: editable === false,
+    tooltip: PropertyTooltip({ tooltip })
+  });
+}
